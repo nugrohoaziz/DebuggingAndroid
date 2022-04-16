64edb377b871953d2133b10c06c26218bee81962
nugrohoaziz <azizagusn17@gmail.com>
2022-04-16 09:49:27 +0700

    add level logging function

--- app/src/main/java/com/example/debuggingandroid/MainActivity.kt
+++ app/src/main/java/com/example/debuggingandroid/MainActivity.kt
@@ -13,7 +13,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun logging(){
-        Log.v(TAG, "Hello Aziz")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
     /**
      * cara melihat di logcat verbose
