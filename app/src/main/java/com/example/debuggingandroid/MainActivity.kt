45c6277b34695a47a816f46999dea216e8d461ae
nugrohoaziz <azizagusn17@gmail.com>
2022-04-18 21:13:13 +0700

    log d for log debug on function devision show the number devided by var denimonator

--- app/src/main/java/com/example/debuggingandroid/MainActivity.kt
+++ app/src/main/java/com/example/debuggingandroid/MainActivity.kt
@@ -9,11 +9,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fourth()
-        second()
-        first()
+        devision()
     }
 
+    /**
+     * learn how to read error code find the error from log "divided by zero"
+     */
+    fun devision(){
+        val numerator = 60
+        var denimonator = 4
+        repeat(5){
+            Log.d(TAG, "${denimonator}")
+            Log.v(TAG, "${numerator / denimonator--}")
+        }
+    }
+
+    /**
+     * learn how look log message level
+     */
+    fun logging(){
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
+    /**
+     * Learn how to devine code run
+     */
     fun first() {
         second()
         Log.v(TAG, "1")
@@ -32,4 +56,7 @@ class MainActivity : AppCompatActivity() {
     fun fourth() {
         Log.v(TAG, "4")
     }
+
+
+
 }
